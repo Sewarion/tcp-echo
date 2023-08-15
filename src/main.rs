cb4c063d4fc6677878d3264540b7fce940cff990
Julian Fiedler <fiedler.julian@web.de>
2023-08-15 23:13:06 +0200

    Added TCP Echo-Server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{Error, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    println!("Incoming connection from:  {}", stream.peer_addr()?);
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write_all(&buf[..bytes_read])?;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8888").expect("Failed to bind socket");
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => {
+                eprintln!("Failed: {}", e)
+            }
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                });
+            }
+        }
+    }
 }
